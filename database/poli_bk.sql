68093bdc80dc3461ab56deb4e41ebb64762e1c3d
Nurul-Isma <nurulismawati236@gmail.com>
2024-01-08 19:59:54 +0700

    Update Database

--- database/poli_bk.sql
+++ database/poli_bk.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 05, 2024 at 02:42 PM
+-- Generation Time: Jan 08, 2024 at 01:59 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -45,7 +45,8 @@ INSERT INTO `daftar_poli` (`id`, `id_pasien`, `id_jadwal`, `keluhan`, `no_antria
 (2, 8, 3, 'saya pusing dok', 1, '0'),
 (3, 9, 1, 'kaki saya sakit dok', 1, '0'),
 (4, 9, 2, 'mata saya sakit dok', 2, '0'),
-(5, 9, 3, 'tes', 2, '1');
+(5, 9, 3, 'tes', 2, '1'),
+(6, 11, 3, 'Saya pusing dok', 3, '1');
 
 -- --------------------------------------------------------
 
@@ -65,7 +66,9 @@ CREATE TABLE `detail_periksa` (
 
 INSERT INTO `detail_periksa` (`id`, `id_periksa`, `id_obat`) VALUES
 (1, 1, 2),
-(2, 1, 7);
+(2, 1, 7),
+(3, 2, 2),
+(5, 2, 11);
 
 -- --------------------------------------------------------
 
@@ -87,11 +90,12 @@ CREATE TABLE `dokter` (
 --
 
 INSERT INTO `dokter` (`id`, `nama`, `password`, `alamat`, `no_hp`, `id_poli`) VALUES
-(1, 'Nurul Ismawati', 'Nurul Ismawati', 'Semarang Barat', '082146005390', 1),
+(1, 'Nurul Ismawati', 'Nurul Ismawati', 'Semarang Barat', '082146005391', 1),
 (2, 'Salim Sulaiman', 'Salim Sulaiman', 'Semarang Tengah', '082157349088', 2),
 (3, 'Dinita Kusumasari', 'Dinita Kusumasari', 'Semarang Selatan', '082190213375', 3),
 (5, 'Ilham Maulana Akbar', 'Ilham Maulana Akbar', 'Semarang Timur', '082137484855', 6),
-(6, 'Raiyan Salmanabil', 'Raiyan Salmanabil', 'Semarang Utara', '084265771238', 1);
+(6, 'Raiyan Salmanabil', 'Raiyan Salmanabil', 'Semarang Utara', '084265771238', 1),
+(7, 'Adam', 'Adam', 'Semarang Tengah', '082159637845', 6);
 
 -- --------------------------------------------------------
 
@@ -104,18 +108,20 @@ CREATE TABLE `jadwal_periksa` (
   `id_dokter` int(11) UNSIGNED NOT NULL,
   `hari` enum('Senin','Selasa','Rabu','Kamis','Jumat','Sabtu') NOT NULL,
   `jam_mulai` time NOT NULL,
-  `jam_selesai` time NOT NULL
+  `jam_selesai` time NOT NULL,
+  `status` enum('0','1') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `jadwal_periksa`
 --
 
-INSERT INTO `jadwal_periksa` (`id`, `id_dokter`, `hari`, `jam_mulai`, `jam_selesai`) VALUES
-(1, 1, 'Rabu', '15:00:00', '18:00:00'),
-(2, 2, 'Kamis', '08:00:00', '11:00:00'),
-(3, 1, 'Senin', '13:00:00', '15:00:00'),
-(4, 1, 'Jumat', '07:05:00', '09:05:00');
+INSERT INTO `jadwal_periksa` (`id`, `id_dokter`, `hari`, `jam_mulai`, `jam_selesai`, `status`) VALUES
+(1, 1, 'Rabu', '15:00:00', '18:00:00', '0'),
+(2, 2, 'Kamis', '08:00:00', '11:00:00', '0'),
+(3, 1, 'Senin', '13:00:00', '15:00:00', '1'),
+(4, 1, 'Jumat', '07:05:00', '09:05:00', '0'),
+(5, 1, 'Jumat', '11:25:00', '01:25:00', '0');
 
 -- --------------------------------------------------------
 
@@ -135,10 +141,9 @@ CREATE TABLE `obat` (
 --
 
 INSERT INTO `obat` (`id`, `nama_obat`, `kemasan`, `harga`) VALUES
-(1, 'Antasida DOEN I tablet kunyah, kombinasi : Alumini', 'ktk 10 x 10 tablet kunyah', 14000),
+(1, 'Antasida DOEN I tablet kunyah, kombinasi ', 'ktk 10 x 10 tablet kunyah', 14000),
 (2, 'Tiamin (Vitamin B1) tablet 50 mg (HCl/Nitrat)', 'btl 1000 tablet', 140000),
 (5, 'Retinol (Vitamin A) 200.000 IU kapsul lunak', 'btl 50 kapsul lunak', 25000),
-(6, 'Povidon lodida larutan 10%', 'btl 1000 ml', 14000),
 (7, 'Difenhidramin injeksi i.m 10 mg/ml/(HCI)', 'ktk 30 amp @ 1 ml', 36000),
 (8, 'Epinefrin (adrenalin) injeksi 0,1 % (sebagai HCL)', 'ktk 30 amp @ 1 ml', 49000),
 (11, 'Zink tablet 20 mg', 'ktk 10 x 10 tablet', 30000),
@@ -169,10 +174,10 @@ INSERT INTO `pasien` (`id`, `nama`, `password`, `alamat`, `no_ktp`, `no_hp`, `no
 (3, 'Pasien C', 'Pasien C', 'Bulustalan, Jawa tengah', '8465239754102015', '082656314990', '202312-003'),
 (4, 'Pasien D', 'Pasien D', 'Sadewa, Kota Semarang', '0147904603581002', '082765342215', '202312-004'),
 (5, 'Samy Sulaiman', 'samsul', 'Balapulang, Kota Tegal', '5493204065700341', '082745963125', '202312-009'),
-(6, 'Pasien X', 'pasienx', 'Semarang Selatan', '5497630210540368', '082165879434', '202312-010'),
 (8, 'Maulana Akbar', 'ilhamakbar', 'Pati', '8764316795621463', '082165479532', '202401-012'),
 (9, 'Pasien ', 'pasien', 'Semarang', '1576249720102365', '082155557823', '202401-013'),
-(10, 'Dinita', 'Dinita', 'Pusponjolo Timur', '2103025810061401', '082564759823', '202401-014');
+(10, 'Dinita', 'Dinita', 'Pusponjolo Timur', '2103025810061401', '082564759823', '202401-014'),
+(11, 'Nanda', 'Nanda', 'Tegal', '12578960174568410', '082145634478', '202401-015');
 
 -- --------------------------------------------------------
 
@@ -193,7 +198,8 @@ CREATE TABLE `periksa` (
 --
 
 INSERT INTO `periksa` (`id`, `id_daftar_poli`, `tgl_periksa`, `catatan`, `biaya_periksa`) VALUES
-(1, 5, '2024-01-05 00:43:00', 'oke', 150000);
+(1, 5, '2024-01-05 00:43:00', 'sip', 150000),
+(2, 6, '2024-01-08 07:06:00', 'Minum obatnya ya', 150000);
 
 -- --------------------------------------------------------
 
@@ -212,10 +218,9 @@ CREATE TABLE `poli` (
 --
 
 INSERT INTO `poli` (`id`, `nama_poli`, `keterangan`) VALUES
-(1, 'Umum', 'Menyediakan layanan pemeriksaan dan pengobatan untuk berbagai masalah kesehatan umum'),
+(1, 'Umum', 'Menyediakan layanan pemeriksaan dan pengobatan untuk berbagai masalah kesehatan umum.'),
 (2, 'Mata', 'Khusus menangani masalah kesehatan mata, seperti pemeriksaan mata, koreksi penglihatan, dan pengobatan penyakit mata'),
 (3, 'Gigi', 'Menyediakan layanan pemeriksaan gigi, perawatan gigi, dan tindakan kedokteran gigi lainnya'),
-(5, 'Kebidanan dan Kandungan', 'Menangani masalah kehamilan, persalinan, dan perawatan kesehatan reproduksi wanita'),
 (6, 'Penyakit Dalam', 'Menangani penyakit-penyakit dalam tubuh, seperti penyakit jantung, diabetes, dan gangguan sistemik lainnya');
 
 --
@@ -235,8 +240,8 @@ ALTER TABLE `daftar_poli`
 --
 ALTER TABLE `detail_periksa`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_detailperiksa_obat` (`id_obat`),
-  ADD KEY `fk_detailperiksa_periksa` (`id_periksa`);
+  ADD KEY `fk_detailperiksa_periksa` (`id_periksa`),
+  ADD KEY `fk_detailperiksa_obat` (`id_obat`);
 
 --
 -- Indexes for table `dokter`
@@ -285,25 +290,25 @@ ALTER TABLE `poli`
 -- AUTO_INCREMENT for table `daftar_poli`
 --
 ALTER TABLE `daftar_poli`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `detail_periksa`
 --
 ALTER TABLE `detail_periksa`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `dokter`
 --
 ALTER TABLE `dokter`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `jadwal_periksa`
 --
 ALTER TABLE `jadwal_periksa`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `obat`
@@ -315,13 +320,13 @@ ALTER TABLE `obat`
 -- AUTO_INCREMENT for table `pasien`
 --
 ALTER TABLE `pasien`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `periksa`
 --
 ALTER TABLE `periksa`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `poli`
@@ -344,7 +349,7 @@ ALTER TABLE `daftar_poli`
 -- Constraints for table `detail_periksa`
 --
 ALTER TABLE `detail_periksa`
-  ADD CONSTRAINT `fk_detailperiksa_obat` FOREIGN KEY (`id_obat`) REFERENCES `obat` (`id`),
+  ADD CONSTRAINT `fk_detailperiksa_obat` FOREIGN KEY (`id_obat`) REFERENCES `obat` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `fk_detailperiksa_periksa` FOREIGN KEY (`id_periksa`) REFERENCES `periksa` (`id`);
 
 --
